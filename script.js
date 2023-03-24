160ef0041346598a9b6cdb4414195f5c8dbb4c37
Abdisalam <abdisalamfresh@gmail.com>
2023-03-24 00:35:51 -0700

    First commit and issue with the stick not being drawn will be fixed shortly all other features are done will be modified and refactored

---
+++ script.js
@@ -0,0 +1,125 @@
+const wordEl = document.getElementById('word')
+const wrongLettersEl = document.getElementById('wrong-letters')
+const playAgainBtn = document.getElementById('play-button')
+const popup = document.getElementById('popup-container')
+const notification = document.getElementById('notification-container')
+const finalMessage = document.getElementById('final-message')
+
+
+const figureParts = document.querySelectorAll('.figure.part');
+
+const words = [
+    'buddy', 
+    'help', 
+    'game', 
+    'programming', 
+    'computers', 
+    'monitors', 
+    'hangman', 
+    'playstation',
+    'notification',
+    'iphone',
+];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const correctLetters = [];
+const wrongLetters = [];
+
+
+// Show hidden word
+function displayWord(){
+    wordEl.innerHTML = `
+    ${selectedWord
+        .split('')
+        .map(letter => `
+            <span class="letter">
+                ${correctLetters.includes(letter) ? letter : ''}
+            </span>
+        `).join('')}
+    `;
+
+    const innerWord = wordEl.innerText.replace(/\n/g, '');
+
+    if (innerWord === selectedWord){
+        finalMessage.innerText = 'Congratulation! You Won! ';
+        popup.style.display = 'flex';
+    }
+}
+// Update the wrong letters 
+function updateWrongLettersEl(){
+    // Display wrong letters
+    wrongLettersEl.innerHTML = `
+        ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''} 
+        ${wrongLetters.map(letter => `<span class="flex"> ${letter} </span>`)}
+    `;
+
+    //Display parts
+    figureParts.forEach((part, index) => {
+        const errors =  wrongLetters.length - figureParts.length;
+
+        if(index < errors){
+            part.style.display = 'block';
+        
+        }else {
+            part.style.display = 'none';
+        }
+    });
+
+    //Check if Lost
+    if (wrongLetters.length === figureParts.length){
+        finalMessage.innerText = 'Unfortunatly you Lost.';
+        popup.style.display = 'flex';
+    }
+}
+// Show notification
+function showNotification(){
+    notification.classList.add('show');
+
+    setTimeout(() => {
+        notification.classList.remove('show')
+    }, 1000)
+}
+
+// KeyDown letter press
+window.addEventListener('keydown', e => {
+    
+    if(e.keyCode >= 65 && e.keyCode <= 90){
+        const letter = e.key;
+
+        if(selectedWord.includes(letter)){
+            if(!correctLetters.includes(letter)){
+                correctLetters.push(letter);
+
+                displayWord();
+            }else {
+                showNotification();
+            }
+        }else {
+            if (!wrongLetters.includes(letter)){
+                wrongLetters.push(letter);
+
+                updateWrongLettersEl();
+            }else {
+                showNotification();
+            }
+        }
+    }
+});
+
+// Restart game and play game
+playAgainBtn.addEventListener('click', () => {
+    //Empty arrays 
+    correctLetters.splice(0);
+    wrongLetters.splice(0);
+
+    selectedWord = words[Math.floor(Math.random() * words.length)];
+
+    displayWord();
+
+    updateWrongLettersEl();
+
+    popup.style.display = 'none'
+})
+
+displayWord();
